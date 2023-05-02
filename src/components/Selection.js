ca3df541321f0fbdf997ffa0bac259ef564aad7e
Rahul Sharma <rahulsh3105@gmail.com>
2023-05-02 10:05:35 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,9 @@
+import React from 'react'
 
+export default function Selection(props) {
+  return (
+    <div>
+       <div style={props.applyColor} className="fix-box">{props.key}</div>
+    </div>
+  )
+}
